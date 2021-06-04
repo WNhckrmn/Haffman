627d19361cb8828219eab7f959b64f43c8193d1f
Семён <65238739+WNhckrmn@users.noreply.github.com>
2021-06-04 11:43:42 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,117 +1,170 @@
+//
+// Created by tiber on 07.05.2021.
+//
+
 #include <iostream>
+#include <vector>
+#include <list>
 #include <map>
 #include <fstream>
 #include <cmath>
 using namespace std;
-class segment{
-public:
-    long double left;
-    long double right;
-    long double freq;
-    segment(){
-        left = 0;
-        right = 0;
-        freq = 0;
-    }
-    segment(long double l, long double r, long double f){
-        left = l;
-        right = r;
-        freq = f;
-    }
+map<char, vector<bool> > table;
+vector<bool> bincode;
+class Uzel{
+    public:
+        int num;
+        char ch;
+        Uzel *left, *right;
+        Uzel(){
+            left=nullptr;
+            right= nullptr;
+            num=0;
+            ch='\0';
+        }
+        ~Uzel(){
+            delete[]left;
+            delete[]right;
+        }
 };
 
-long double min_mantis(long double l , long double r){
-    long double count = 0;
-    int a = floor(l * 10* count);
-    int b = floor(r * 10 * count);
-    while (b==a){
-        a = floor(l * 10* count);
-        b = floor(r * 10 * count);
-        count ++;
+
+struct MyCompare
+{
+    bool operator()(Uzel* l,Uzel* r) const
+    {return l->num < r->num;}
+};
+
+void searchword(Uzel* tree)
+{
+
+    if (tree->left!=nullptr){
+        bincode.push_back(0);
+        searchword(tree->left);
     }
-    return count;
+
+    if(tree->right != nullptr){
+        bincode.push_back(1);
+        searchword(tree->right);
+    }
+    if(tree->ch) {
+        table[tree->ch] = bincode;
+    }
+    bincode.pop_back();
+
 }
-int main() {
-    int M = 15;
-    map < char, long double > v;
+
+int main()
+{
+    map < char, int > m;
     int cc;
-    long double words = 0;
     string s;
     ifstream fail;
-    fail.open("C:\\Users\\tiber\\CLionProjects\\ariphmetic\\text.txt");
+    fail.open("C:\\Users\\tiber\\CLionProjects\\untitled\\text.txt");
     if (!(fail.is_open())) // если файл не открыт
         cout << "file can't open\n"; // сообщить об этом
     else{
-        while((cc = fail.get()) != EOF) {
-            v[char(cc)]++;
-            words++;
+        while((cc = fail.get()) != EOF) {  
+            m[char(cc)]++;
         }
     }
     fail.close();
 
-    map < char,long double> ::iterator ii;
-    map <char, segment > table;
-    long double st = 0;
-    int countt = 0;
-    for (ii = v.begin(); ii !=v.end() ; ++ii) {
-        countt++;
-        table[ii->first].freq=(ii->second)/words;
-        table[ii->first].left = st;
-        table[ii->first].right = st+ table[ii->first].freq;
-        st = table[ii->first].right;
+
+
+    map < char, int> :: iterator ii;
+
+
+    list< Uzel* > L;
+
+    for(ii=m.begin();ii!=m.end();ii++){
+        Uzel* tr = new Uzel;
+        tr->num=ii->second;
+        tr->ch=ii->first;
+        tr->left= nullptr;
+        tr->right = nullptr;
+        L.push_back(tr);
     }
-    map < char, segment > ::iterator iip;
-//    for (iip = table.begin(); iip !=table.end() ; ++iip) {
-//        cout<<table[iip->first].left<<"--"<<table[iip->first].right<<"::"<<table[iip->first].freq<<endl;
-//    }
 
-    long double l = 0;
-    long double r = 1;
+    while(L.size()!=1)
+    {
+        L.sort(MyCompare());
+        Uzel* fir=L.front();
+        L.pop_front();
+        Uzel* sec=L.front();
+        L.pop_front();
+        Uzel* tree = new Uzel;
+        tree->left=fir;
+        tree->right= sec;
+        tree->num=fir->num+sec->num;
+        tree->ch= '\0';
+        L.push_front(tree);
+    }
+    Uzel* root = L.front();
 
-    ifstream file;
-    char c;
-    int x = 0;
-    int count = 0;
-    file.open("C:\\Users\\tiber\\CLionProjects\\ariphmetic\\text.txt");
-    ofstream res("C:\\Users\\tiber\\CLionProjects\\ariphmetic\\result.txt",ios::binary);
-    if (!(file.is_open())) // если файл не открыт
+
+    searchword(root);
+    int words = 0;
+    ofstream res("C:\\Users\\tiber\\CLionProjects\\untitled\\result.bin",ofstream::binary);
+    int word;
+    for ( ii = m.begin(); ii !=m.end() ; ++ii) {
+        words++;
+    }
+    res<<words;
+    for (ii = m.begin(); ii !=m.end() ; ++ii) {
+        res<<(ii->first);
+        res<<(ii->second);
+    }
+
+//    res.open("C:\\Users\\tiber\\CLionProjects\\untitled\\result.bin",ofstream::binary);
+//    if (!(res.is_open())) // если файл не открыт
+//        cout << "file <<res.txt>> can't open\n"; // сообщить об этом
+
+    ifstream fale;
+    fale.open("C:\\Users\\tiber\\CLionProjects\\untitled\\text.txt");
+
+
+    if (!(fale.is_open())) // если файл не открыт
         cout << "file can't open\n"; // сообщить об этом
-    else if(!(res.is_open()))
-        cout<<"file can't open\n";
-    else{
-        res<<M;
-        //int M_last = words % 15;
-        res<<" ";
-        res<<words;
-        res<<" ";
-        res<<countt;
-        map<char, segment> :: iterator iii;
-        for (iii = table.begin(); iii !=table.end() ; ++iii) {
-            res<<iii->first;
-            res<<iii->second.freq;
-            res<<" ";
-        }
-        while (!file.eof()) {
-            char c = file.get();
-            long double l1 = l + table[c].left * (r - l);
-            long double r1 = l + table[c].right * (r - l);
-            l = l1;
-            r = r1;
-            count++;
-            if (count == M) {
-                count = 0;
-//                cout<<l<<endl;
-//                cout<<r<<endl;
-//                int el = min_mantis(l,r);
-//                double plus = 1 / pow(10,el) ;
-                //l+=plus;
-                res << l;
+    else {
+        int count_bit = 0;
+        while (!fale.eof()) {
+            vector<bool>::iterator iiVV;
+            int wort = fale.get();
+            for (iiVV = table[char(wort)].begin(); iiVV != table[char(wort)].end(); ++iiVV) {
+                count_bit++;
             }
         }
-        if (count!=0){
-            res<<l;
-        }
+        res << (count_bit);
+        fale.close();
     }
-    file.close();
+    ifstream falen;
+    falen.open("C:\\Users\\tiber\\CLionProjects\\untitled\\text.txt");
+
+    if (!(falen.is_open())) // если файл не открыт
+        cout << "file can't open\n"; // сообщить об этом
+    else{
+        char binc=0;
+        int count = 0;
+        while(!falen.eof()) {  
+            word = falen.get();
+            vector< bool> :: iterator iiV;
+            for ( iiV = table[char(word)].begin(); iiV !=table[char(word)].end() ; ++iiV) {
+                binc |= (*iiV)<<(7- count);
+                count++;
+                if((count == 8)){
+                    count = 0;
+                    res<<binc;
+                    binc = 0;
+                }
+            }
+        }
+        res<<binc;
+        res<<count;
+    }
+    fale.close();
     res.close();
+
+
+    return 0;
 }
